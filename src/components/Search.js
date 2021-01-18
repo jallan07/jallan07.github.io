3bc5a6ea12e640850a1e76623530ba9849cd03b7
Josh Allan <allan.josh07@gmail.com>
2021-01-18 15:46:10 -0500

    Updated class to className in the Search component.

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,10 +4,10 @@ function Search({ onChange }) {
   return (
     <div className="mx-auto text-center">
       <h5 className="d-inline mr-2">Project Search</h5>
-      <i class="fab fa-searchengin d-inline mr-3"></i>
+      <i className="fab fa-searchengin d-inline mr-3"></i>
       <input
         onChange={onChange}
-        class="d-inline form-control col-md-8 mb-3"
+        className="d-inline form-control col-md-8 mb-3"
         type="search"
         placeholder="Enter a project name or technology..."
         aria-label="Search"
