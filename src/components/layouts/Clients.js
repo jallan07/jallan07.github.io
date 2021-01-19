9d785a8b05fb72369a4644735a3cf8596d2e73ec
Josh Allan <allan.josh07@gmail.com>
2021-01-19 18:23:31 -0500

    Updated the Clients.js component section wrapper to fix white space showing at the bottom.

--- src/components/layouts/Clients.js
+++ src/components/layouts/Clients.js
@@ -3,7 +3,7 @@ import Client from '../Client';
 
 function Clients() {
   return (
-    <section class=" wrapper">
+    <section class="accordion-section row wrapper">
       <section
         class="container main-container col-md-9 col-xs-12 offset-1"
         data-aos="fade-up"
